a607a0a58b02e862588a515671a47fe0ddeeea60
QuintenLemoine <qlemoine@aischool.org>
2025-10-03 00:42:14 -0400

    Configure ALLOWED_HOSTS and DEBUG for PythonAnywhere

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q!c-)3vym4bx(q*jjcort!r1t=(+fj63#kg7eys-lb0c536se-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['qklemoine.pythonanywhere.com']
 
 
 # Application definition
